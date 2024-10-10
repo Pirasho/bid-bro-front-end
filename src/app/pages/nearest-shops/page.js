e9a702a1c9e8fea1113ff5ac150c48cd2e79f6db
Neluxcy <Nelaxsana@gmail.com>
2024-10-10 17:08:12 +0530

    gps

---
+++ src/app/pages/nearest-shops/page.js
@@ -0,0 +1,13 @@
+// pages/nearest-shops.js
+import GeoLocationMap from "../../components/GeoLocationMap";
+
+const NearestShops = () => {
+  return (
+    <div>
+      <h1>Find Nearest Shops</h1>
+      <GeoLocationMap />
+    </div>
+  );
+};
+
+export default NearestShops;
